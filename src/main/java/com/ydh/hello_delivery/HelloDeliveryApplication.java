cbb35a8bd11def723334fb9a27fd8d19c6d11a3b
ydh6226 <ydh6226@naver.com>
2021-01-31 18:13:50 +0900

    [ADD] 프로젝트 기본 설정

---
+++ src/main/java/com/ydh/hello_delivery/HelloDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.ydh.hello_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ydh/hello_delivery/HelloDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydh.hello_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
